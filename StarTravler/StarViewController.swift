3164c8ca790e402036144503abae724aded16fb8
EDUARDO MENDOZA <rl238513@rlas-macair-25.local>
2019-02-01 11:55:07 -0600

    hello

---
+++ StarTravler/StarViewController.swift
@@ -0,0 +1,21 @@
+//
+//  StarViewController.swift
+//  StarTravler
+//
+//  Created by EDUARDO MENDOZA on 2/1/19.
+//  Copyright © 2019 EDUARDO MENDOZA. All rights reserved.
+//
+
+import UIKit
+
+class StarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+}
--- StarTravler/ViewController.swift
+++ StarTravler/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var locationLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func rDwarf(_ sender: Any) {
+        performSegue(withIdentifier: "showStarViewController", sender: nil)
+    }
+    
+    @IBAction func bDwarf(_ sender: Any) {
+        performSegue(withIdentifier: "showStarViewController", sender: nil)
+    }
+    
+    
+    
+    
 }
 
